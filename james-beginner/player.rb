8c413353f8baf9bce2cdc84bdf36faeaa1b3672d
Michael Mitchell <michaelmitchell@hush.com>
2012-05-01 08:18:21 -0700

    create @warrior instance variable

--- james-beginner/player.rb
+++ james-beginner/player.rb
@@ -2,53 +2,54 @@ require 'ruby-debug'
 
 class Player
   def play_turn(warrior) 
+    @warrior = warrior
     @health ||= warrior.health
-    warrior.determine_move(@health)
+    determine_move
     @health = warrior.health
   end
+
   def should_back_up?
-    if health <= 10
-      if feel.enemy? and feel.character != "a"
+    if @warrior.health <= 10
+      if @warrior.feel.enemy? and @warrior.feel.character != "a"
         true 
       end
     end
   end
 
-  def should_rest?(previous_health)
-    true if (health < 20 and feel.empty? and !taking_damage?(previous_health))
+  def should_rest?
+    true if (@warrior.health < 20 and @warrior.feel.empty? and !taking_damage?)
   end
 
-  def taking_damage?(previous_health)
-    debugger
-    true if previous_health - health > 0
+  def taking_damage?
+    true if @health - @warrior.health > 0
   end
 
-  def determine_move(previous_health)
-    walk!(:backward) and return if should_back_up?
-    rest! and return if should_rest?(previous_health)
-    if previous_health - self.health == 0
-      if health < 20
+  def determine_move
+    @warrior.walk!(:backward) and return if should_back_up?
+    @warrior.rest! and return if should_rest?
+    if @health - @warrior.health == 0
+      if @warrior.health < 20
         rest!
         return
-      elsif feel.empty?
-        walk!
+      elsif @warrior.feel.empty?
+        @warrior.walk!
         return
       else
-        attack!
+        @warrior.attack!
         return
       end
-    elsif feel.empty?
-      walk!
+    elsif @warrior.feel.empty?
+      @warrior.walk!
       return
     else
-      if health <= 10
-        walk!(:backward)
+      if @warrior.health <= 10
+        @warrior.walk!(:backward)
         return
       end
-      attack!
+      @warrior.attack!
       return
     end
-    previous_health = health
+    @health = @warrior.health
   end
 end
 
